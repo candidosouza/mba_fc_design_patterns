4a6c8afe57e92470796576dd5820d6c4903251df
candidosouza <candidosouzza@gmail.com>
2023-05-30 21:20:04 -0300

    test: integration test generate invoce

--- src/core/tests/integration/application/test_integr_generate_invoices.py
+++ src/core/tests/integration/application/test_integr_generate_invoices.py
@@ -7,11 +7,36 @@ class TestGeneratorInvocesIntegr(unittest.TestCase):
     def setUp(self):
         self.generate_invoices = GenerateInvoices()
 
-    def test_should_generate_invoices(self):
-        output = self.generate_invoices.execute()
+    def test_should_generate_of_invoices_based_on_cash_basis_accounting(self):
+        input_parans = {
+            'month': 1,
+            'year': 2022,
+            'type': "cash"
+        }
+        output = self.generate_invoices.execute(input_parans)
         print('\n')
-        print("###############################################")
+        print('###############################')
         print(output)
-        print("###############################################")
         print('\n')
-        self.assertTrue(True)
+        # self.assertEqual(output[0].date, '05/01/2022')
+        # self.assertEqual(output[0].amout, 6000)
+
+    def test_should_generate_of_invoices_based_on_accrual_basis_accounting(self):
+        input_parans = {
+            'month': 1,
+            'year': 2022,
+            'type': "accrual"
+        }
+        output = self.generate_invoices.execute(input_parans)
+        self.assertEqual(output[0].date, '01/02/2022')
+        self.assertEqual(output[0].amout, 500)
+
+    def test_should_generate_of_invoices_based_on_accrual_basis_accounting_02(self):
+        input_parans = {
+            'month': 2,
+            'year': 2022,
+            'type': "accrual"
+        }
+        output = self.generate_invoices.execute(input_parans)
+        self.assertEqual(output[0].date, '01/02/2022')
+        self.assertEqual(output[0].amout, 500)
