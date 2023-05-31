af973e5f700255b7b8f49dae284fd3fdb10454b3
candidosouza <candidosouzza@gmail.com>
2023-05-31 20:25:17 -0300

    feat: change use case with DIP

--- src/core/invoice/application/use_cases.py
+++ src/core/invoice/application/use_cases.py
@@ -4,37 +4,30 @@ from dateutil.relativedelta import relativedelta
 
 from core.invoice.application.dto import InvoicesOutput
 from core.invoice.application.repository.contract_repository import ContractRepository
-from core.invoice.infra.repository.contract_database_repository import ContractDatabaseRepository
 
 
 @dataclass(slots=True, frozen=True)
-class GenerateInvoices:
-    db_config = {
-        'host': 'db',
-        'port': '5432',
-        'database': 'invoice',
-        'user': 'postgres',
-        'password': 'root'
-    }
+class GenerateInvoices():
 
-    def execute(self, input: 'Input') -> 'Output':
-        contracts_repository = ContractDatabaseRepository()
-        contracts_results, payment_results = contracts_repository.list()
+    contracts_repository: ContractRepository
+
+    def execute(self, input_params: 'Input') -> 'Output':
+        contracts_results, payment_results = self.contracts_repository.list()
         output: InvoicesOutput = []
         for contract in contracts_results:
-            if input['type'] == 'cash':
+            if input_params['type'] == 'cash':
                 for payment in payment_results:
-                    if payment[3].month + 1 != input['month'] or payment[3].year != input['year']:
+                    if payment[3].month + 1 != input_params['month'] or payment[3].year != input_params['year']:
                         output.append(GenerateInvoices.Output(
                             date=payment[3].strftime("%d/%m/%Y"),
                             amout=payment[2]
                         ))
-            if input['type'] == 'accrual':
+            if input_params['type'] == 'accrual':
                 period = 0
                 while period <= contract[3]:
                     period+=1
                     date = contract[4] + relativedelta(months=period)
-                    if date.month + 1 != input['month'] or date.year != input['year']:
+                    if date.month + 1 != input_params['month'] or date.year != input_params['year']:
                         amount = contract[2] / contract[3]
                         output.append(GenerateInvoices.Output(
                             date=date.strftime("%d/%m/%Y"),
@@ -57,4 +50,4 @@ input_parans = {
     'year': 2022,
     'type': "cash"
 }
-GenerateInvoices().execute(input_parans)
+# GenerateInvoices().execute(input_parans)
