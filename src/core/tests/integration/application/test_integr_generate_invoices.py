a2236df106b94086a43449b6c1f22e27bcf1164c
candidosouza <candidosouzza@gmail.com>
2023-06-12 16:46:12 -0300

    test: integration test with database adapter

--- src/core/tests/integration/application/test_integr_generate_invoices.py
+++ src/core/tests/integration/application/test_integr_generate_invoices.py
@@ -1,16 +1,33 @@
 import unittest
 
 from core.invoice.application.use_cases import GenerateInvoices
+from core.invoice.infra.database.database_adapter_factory import ConcreteDatabaseAdapterFactory
 from core.invoice.infra.repository.contract_in_memory_repository import ContractInMemoryRepository
 from core.invoice.infra.repository.contract_database_repository import ContractDatabaseRepository
 
 class TestGeneratorInvocesIntegr(unittest.TestCase):
 
     def setUp(self):
-        self.contract_repository = ContractInMemoryRepository()
-        self.generate_invoices = GenerateInvoices(self.contract_repository) # pylint: disable=abstract-class-instantiated
-        # self.contract_repository = ContractDatabaseRepository()
+        self.adapter_factory = ConcreteDatabaseAdapterFactory()
+        db_config = {
+            'db_type': 'postgresql',
+            'host': 'db',
+            'port': '5432',
+            'database': 'invoice',
+            'user': 'postgres',
+            'password': 'root'
+        }
+        self.postgres_adapter = self.adapter_factory.create_adapter(**db_config)
+        self.postgres_adapter.connect()
+
+        # self.contract_repository = ContractInMemoryRepository()
         # self.generate_invoices = GenerateInvoices(self.contract_repository) # pylint: disable=abstract-class-instantiated
+
+        self.contract_repository = ContractDatabaseRepository(self.postgres_adapter)
+        self.generate_invoices = GenerateInvoices(self.contract_repository) # pylint: disable=abstract-class-instantiated
+
+    def tearDown(self) -> None:
+        self.postgres_adapter.close()
 
     def test_should_generate_of_invoices_based_on_cash_basis_accounting(self):
         input_parans = {
