e54885eccba859d9c28c605bfe6a748c0bc6a361
candidosouza <candidosouzza@gmail.com>
2023-06-12 19:09:45 -0300

    feat: entity interface

---
+++ src/core/shared/domain/entities.py
@@ -0,0 +1,16 @@
+from abc import ABC
+from dataclasses import dataclass, field
+
+from core.shared.domain.value_objects import UniqueEntityId
+
+
+@dataclass(frozen=True, slots=True)
+class Entity(ABC):
+
+    unique_entity_id: UniqueEntityId = field(
+        default_factory=lambda: UniqueEntityId()  # pylint: disable=unnecessary-lambda
+    )
+
+    @property
+    def id(self):  # pylint: disable=invalid-name
+        return str(self.unique_entity_id)
