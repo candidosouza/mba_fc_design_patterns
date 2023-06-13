8eeeb13bcaa0360a0c267ffadc6f7eb7d1148724
candidosouza <candidosouzza@gmail.com>
2023-06-13 15:56:21 -0300

    refactor: refactor use case

--- src/core/invoice/application/use_cases.py
+++ src/core/invoice/application/use_cases.py
@@ -12,23 +12,23 @@ class GenerateInvoices():
     contracts_repository: ContractRepository
 
     def execute(self, input_params: 'Input') -> 'Output':
-        contracts_results, payment_results = self.contracts_repository.list()
+        contracts_results = self.contracts_repository.list()
         output: InvoicesOutput = []
         for contract in contracts_results:
             if input_params['type'] == 'cash':
-                for payment in payment_results:
-                    if payment[3].month + 1 != input_params['month'] or payment[3].year != input_params['year']:
+                for payment in contract.payments:
+                    if payment.date.month + 1 != input_params['month'] or payment.date.year != input_params['year']:
                         output.append(GenerateInvoices.Output(
-                            date=payment[3].strftime("%d/%m/%Y"),
-                            amout=payment[2]
+                            date=payment.date.strftime("%d/%m/%Y"),
+                            amout=payment.amount
                         ))
             if input_params['type'] == 'accrual':
                 period = 0
-                while period <= contract[3]:
+                while period <= contract.periods:
                     period+=1
-                    date = contract[4] + relativedelta(months=period)
+                    date = contract.date + relativedelta(months=period)
                     if date.month + 1 != input_params['month'] or date.year != input_params['year']:
-                        amount = contract[2] / contract[3]
+                        amount = contract.amount / contract.periods
                         output.append(GenerateInvoices.Output(
                             date=date.strftime("%d/%m/%Y"),
                             amout=amount
@@ -44,3 +44,44 @@ class GenerateInvoices():
     @dataclass(slots=True, frozen=True)
     class Output(InvoicesOutput):
         pass
+
+
+
+
+# from core.invoice.infra.repository.contract_database_repository import ContractDatabaseRepository
+# from core.invoice.infra.repository.contract_in_memory_repository import ContractInMemoryRepository
+# from core.invoice.infra.database.database_adapter_factory import ConcreteDatabaseAdapterFactory
+
+# adapter_factory = ConcreteDatabaseAdapterFactory()
+# db_config = {
+#     'db_type': 'postgresql',
+#     'host': 'db',
+#     'port': '5432',
+#     'database': 'invoice',
+#     'user': 'postgres',
+#     'password': 'root'
+# }
+# postgres_adapter = adapter_factory.create_adapter(**db_config)
+# postgres_adapter.connect()
+
+# input_parans = {
+#     'month': 1,
+#     'year': 2022,
+#     'type': "cash"
+# }
+# contract = ContractDatabaseRepository(postgres_adapter)
+# invoices = GenerateInvoices(contract)
+# print('\n')
+# print('database ##############################################')
+# print(invoices.execute(input_parans))
+# print('##############################################')
+# print('\n')
+
+
+# contract_in_memory_repository = ContractInMemoryRepository()
+# invoices = GenerateInvoices(contract_in_memory_repository)
+# print('\n')
+# print('Memory ##############################################')
+# print(invoices.execute(input_parans))
+# print('##############################################')
+# print('\n')
