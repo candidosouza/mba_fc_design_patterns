f34a2c256775d84656e6e0e90972fe7b67c823db
candidosouza <candidosouzza@gmail.com>
2023-06-13 15:53:51 -0300

    feat: add typing contract repository

--- src/core/invoice/application/repository/contract_repository.py
+++ src/core/invoice/application/repository/contract_repository.py
@@ -1,9 +1,12 @@
 from abc import ABC
+from ast import List
 
+from core.invoice.domain.contract import Contract
 
+CT = list[Contract]
 class ContractRepository(ABC):
     
-    def list(self):
+    def list(self) -> CT:
         raise NotImplementedError
     
     def create(self, items):
