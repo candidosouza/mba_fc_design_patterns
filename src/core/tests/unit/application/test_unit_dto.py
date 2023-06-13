1e08c6c6b02b4093533df0e9af58e0fc8bcb7ae7
candidosouza <candidosouzza@gmail.com>
2023-06-13 10:50:29 -0300

    test: unit test invoices output

---
+++ src/core/tests/unit/application/test_unit_dto.py
@@ -0,0 +1,18 @@
+import unittest
+from dataclasses import is_dataclass
+from core.invoice.application.dto import InvoicesOutput
+
+
+class TestInvoicesOutput(unittest.TestCase):
+
+    def test_is_dataclass(self):
+        self.assertTrue(is_dataclass(InvoicesOutput))
+        self.assertTrue(InvoicesOutput.__dataclass_fields__)
+        self.assertTrue(InvoicesOutput.__dataclass_params__)
+
+    def test_fields(self):
+        self.assertEqual(InvoicesOutput.__slots__, ('date', 'amout'))
+        self.assertEqual(InvoicesOutput.__annotations__, {
+            'date': str,
+            'amout': int
+        })
