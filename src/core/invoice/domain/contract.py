e23058c347901eeff4ae5ee1528d74a827af5710
candidosouza <candidosouzza@gmail.com>
2023-06-14 12:42:28 -0300

    feat: add ocp solid, strategy and simple factory patterns

--- src/core/invoice/domain/contract.py
+++ src/core/invoice/domain/contract.py
@@ -1,11 +1,11 @@
+from abc import ABC, abstractmethod
 import datetime
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Any, List, Optional
 from dateutil.relativedelta import relativedelta
 from core.invoice.domain.invoice import Invoice
-
-from core.shared.domain.entities import Entity
 from core.invoice.domain.payment import Payment
+from core.shared.domain.entities import Entity
 
 
 @dataclass(slots=True, kw_only=True, frozen=True)
@@ -22,27 +22,53 @@ class Contract(Entity):
     def add_payment(self, payment: Payment):
         self.payments.append(payment)
     
-    def get_payments(self):
+    def get_payments(self) -> List[Payment]:
         return self.payments
     
-    def generate_invoices(self, mouth: int, year: int, type_transaction: str):
-        invoices: Invoice = []
-        if type_transaction == 'cash':
-            for payment in self.get_payments():
-                if payment.date.month + 1 != mouth or payment.date.year != year:
-                    invoices.append(Invoice(
-                        date=payment.date.strftime("%d/%m/%Y"),
-                        amount=payment.amount
-                    ))
-        if type_transaction == 'accrual':
-            period = 0
-            while period <= self.periods:
-                period+=1
-                date = self.date + relativedelta(months=period)
-                if date.month + 1 != mouth or date.year != year:
-                    amount = self.amount / self.periods
-                    invoices.append(Invoice(
-                        date=date.strftime("%d/%m/%Y"),
-                        amount=amount
-                    ))
+    def generate_invoices(self, mouth: int, year: int, type: str) -> List[Any]:
+        invoice_generation_strategy = InvoiceGenerationFactory.create(type)
+        return invoice_generation_strategy.generate(self, mouth, year)
+
+
+class InvoiceGenerationFactory:
+    @staticmethod
+    def create(type: str):
+        if type == 'accrual':
+            return AccrualInvoiceGenerationStrategy()
+        if type == 'cash':
+            return CashInvoiceGenerationStrategy()
+        raise Exception('Invalid invoice generation strategy')
+
+
+class InvoiceGenerationStrategy(ABC):
+    @abstractmethod
+    def generate(self, contract: Contract, month: int, year: int) -> List[Invoice]:
+        return NotImplemented
+
+
+class CashInvoiceGenerationStrategy(InvoiceGenerationStrategy):
+    def generate(self, contract: Contract, month: int, year: int) -> List[Invoice]:
+        invoices: List[Invoice] = []
+        for payment in contract.get_payments():
+            if payment.date.month + 1 != month or payment.date.year != year:
+                invoices.append(Invoice(
+                    date=payment.date.strftime("%d/%m/%Y"),
+                    amount=payment.amount
+                ))
+        return invoices
+
+
+class AccrualInvoiceGenerationStrategy(InvoiceGenerationStrategy):
+    def generate(self, contract: Contract, month: int, year: int) -> List[Invoice]:
+        invoices: List[Invoice] = []
+        period = 0
+        while period <= contract.periods:
+            period+=1
+            date = contract.date + relativedelta(months=period)
+            if date.month + 1 != month or date.year != year:
+                amount = contract.amount / contract.periods
+                invoices.append(Invoice(
+                    date=date.strftime("%d/%m/%Y"),
+                    amount=amount
+                ))
         return invoices
