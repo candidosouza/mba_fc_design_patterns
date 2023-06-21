09613e63312afa0b1c739a0f4934bd2e54a37662
candidosouza <candidosouzza@gmail.com>
2023-06-21 14:56:12 -0300

    feat: config send and logger email

--- src/core/app.py
+++ src/core/app.py
@@ -1,10 +1,12 @@
-import re
+from core.invoice.application.usecase.dto import EmailOutput
+from core.invoice.application.usecase.email_sender import EmailLogger, EmailSender
 from flask import Flask
 from flask import request
 from core.invoice.application.decorator.logger_decorator import LoggerDecorator
 from core.invoice.application.usecase.generate_invoices import GenerateInvoices
 from core.invoice.infra.database.database_adapter_factory import ConcreteDatabaseAdapterFactory
 from core.invoice.infra.repository.contract_database_repository import ContractDatabaseRepository
+from core.invoice.infra.mediator.concrete_mediator import ConcreteMediator
 
 app = Flask(__name__)
 
@@ -23,6 +25,8 @@ def generate_invoices():
     postgres_adapter = adapter_factory.create_adapter(**db_config)
     postgres_adapter.connect()
     contract_repository = ContractDatabaseRepository(postgres_adapter)
+    mediator = ConcreteMediator()
+    mediator.subscribe('invoices_generated', lambda data: print(data))
     generate_invoices = LoggerDecorator(GenerateInvoices(contract_repository))
     output = generate_invoices.execute(request.args)
     if request.method == 'POST':
